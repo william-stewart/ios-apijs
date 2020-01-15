bc8f2e826439a6ba2e5d6cbaede9fdf4d5101fd6
William Stewart <williamjstew@gmail.com>
2020-01-15 15:35:05 -0800

    Cleaned up, basic db communication

--- Server.js
+++ Server.js
@@ -6,29 +6,20 @@ app.use(bodyParser.urlencoded({ extended: false}));
 const mysql = require('mysql');
 let db = require('./tools/db.js');
 
-app.get('/',function(req,res) {
-  res.send("hello from server 2 home page");
-})
-
 app.get('/ios-api',function(req,res) {
   res.send({"result": "ios-api base"});
 })
 
 app.get('/ios-api/testjson',function(req,res) {
-  //res.json({"test response": "successful call to testjson"});
+  res.json({"test response": "successful call to testjson"});
+})
+
+app.get('/ios-api/testdb',function(req,res) {
   db.getDataFromDb(function(err,results) {
     if(err) { res.status(500).send("Server error"); return; } 
     res.json(results);
   });
 })
-
-app.get('/ios-api/testdb'),function(req,res) {
-  console.log('received call to testdb');
-  db.getDataFromDb(function(err,results) {
-    if(err) { res.status(500).send("Server error"); return; } 
-    res.json(results);
-  });
-}
 
 app.use('/ios-api/*',function(req, res){
   res.send('Error 404: Not Found in ios-api');
