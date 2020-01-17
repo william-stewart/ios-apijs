64423bf9f35e75182fd375edfa36f8d0e0df3346
William Stewart <williamjstew@gmail.com>
2020-01-16 16:59:32 -0800

    added parameter functionality

--- Server.js
+++ Server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 const mysql = require('mysql');
 let db = require('./tools/db.js');
 
@@ -10,8 +11,47 @@ app.get('/ios-api',function(req,res) {
   res.send({"result": "ios-api base"});
 })
 
+app.post('/ios-api/addmsg',function(req,res) {
+  var params = {userID: req.body.userID,
+                messageTitle: req.body.messageTitle,
+                message: req.body.message,
+                posted: Date.now()}
+  console.log(params);
+  
+  db.addMessage(params,function(err,results) {
+    if(err) { res.status(500).send("Server error"); return; } 
+    res.json({"results": results});
+  });
+  
+})
+
+app.get('/ios-api/allmessages',function(req,res) {
+  db.getAllMessages(function(err,results) {
+    if(err) { res.status(500).send("Server error"); return; } 
+    res.json({"results": results});
+  });
+})
+
+app.get('/ios-api/msg/:userId',function(req,res) {
+  var params = {user: req.params.userId};
+
+  db.getMessagesByUser(params,function(err,results) {
+    if(err) { res.status(500).send("Server error"); return; } 
+    res.json({"results": results});
+  });
+})
+
+app.get('/ios-api/board/:boardName',function(req,res) {
+  var params = {boardName: req.params.boardName};
+
+  db.getMessagesByBoard(params,function(err,results) {
+    if(err) { res.status(500).send("Server error"); return; } 
+    res.json({"results": results});
+  });
+})
+
 app.get('/ios-api/testjson',function(req,res) {
-  res.json({"test response": "successful call to testjson"});
+  res.json({"test_response": "successful call to testjson"});
 })
 
 app.get('/ios-api/testdb',function(req,res) {
--- tools/db.js
+++ tools/db.js
@@ -9,6 +9,58 @@ var pool = mysql.createPool({
     supportBigNumbers: process.env.DBSUPPORTBIGNUMBERS
 });
 
+
+
+exports.getAllMessages = function(callback) {
+    const queryString = "SELECT userID,boardName,messageTitle,message,postedDate from messageboard";
+
+    pool.getConnection(function(err, connection) {
+        if (err) {console.log(err); callback(true); return;}
+        connection.query(queryString, function(err, results) {
+            connection.release();
+            if(err) {console.log(err); callback(true); return;}
+            callback(false, results);
+        })
+    })
+};
+
+exports.getMessagesByUser = function(params,callback) {
+    const queryString = "SELECT userID,boardName,messageTitle,message,postedDate from messageboard where userID = ?";
+    pool.getConnection(function(err, connection) {
+        if (err) {console.log(err); callback(true); return;}
+        connection.query(queryString,[params.user], function(err, results) {
+            connection.release();
+            if(err) {console.log(err); callback(true); return;}
+            callback(false, results);
+        })
+    })
+};
+
+exports.getMessagesByBoard = function(params,callback) {
+    const queryString = "SELECT userID,boardName,messageTitle,message,postedDate from messageboard where boardName = ?";
+    pool.getConnection(function(err, connection) {
+        if (err) {console.log(err); callback(true); return;}
+        connection.query(queryString,[params.boardName], function(err, results) {
+            connection.release();
+            if(err) {console.log(err); callback(true); return;}
+            callback(false, results);
+        })
+    })
+};
+
+exports.addMessage = function(params,callback) {
+    const queryString = "INSERT into messageboard values (? , ? , ? , ?)";
+
+    pool.getConnection(function(err, connection) {
+        if (err) {console.log(err); callback(true); return;}
+        connection.query(queryString,[params.userID,params.messageTitle,params.message,params.posted], function(err, results) {
+            connection.release();
+            if(err) {console.log(err); callback(true); return;}
+            callback(false, results);
+        })
+    })
+};
+
 exports.getDataFromDb = function(callback) {
     const queryString = "SELECT * from leaderboard where position >= 1 and position <= 5";
 
